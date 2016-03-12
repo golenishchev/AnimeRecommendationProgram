bf0f7052c67462f96e6df03169172136475d64be
Yevhen Holienishchev <Yevhen Holienishchev>
2016-03-12 21:44:20 +0200

    changed text in achivements

--- Achivement.java
+++ Achivement.java
@@ -8,9 +8,9 @@ public class Achivement {
         if (titlesCount == 1) {
             System.out.println("New Achivement: First step of Otaku");
         } else if (titlesCount >= 10) {
-            System.out.println("New Achivement: Big eyes lover");
+            System.out.println("New Achivement: Casual wather");
         } else if (titlesCount >= 100) {
-            System.out.println("New Achivement: Big -oppai- eyes lover");
+            System.out.println("New Achivement: Big eyes lover");
         } else if (titlesCount >= 300) {
             System.out.println("New Achivement: Millionaire. You didn't pirated all this anime? =)");
         } else if (titlesCount >= 1000) {
